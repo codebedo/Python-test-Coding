956dc98697de7ba27542d09a19b93c9d52efd246
codebedo <bedirahan13@gmail.com>
2025-06-20 19:30:15 +0300

    biraz daha iyi

--- CharStats.py
+++ CharStats.py
@@ -1,15 +1,21 @@
 class StatsOfChar:
-    def character(self, char, puan, unvan):
+    def __int__(self, char, puan):
         self.char = char
         self.puan = puan
-        self.unvan = unvan
+
 
     def get_unvan(self):
 
-        if self.puan < 3 :
-            return f"{self.char}'un {self.puan}'ı bu ve {self.unvan} 'a sahip"
-        if self.puan < 5 and  self.puan > 3:
-            return f"{self.char}'un {self.puan}'ı bu ve {self.unvan} 'a sahip"
+        if self.puan < 3:
+            return "Acemi"
+        elif 3 <= self.puan <5:
+            return "Usta"
 
-        if self.puan > 4:
-            return f"{self.char}'un {self.puan}'ı bu ve {self.unvan} 'a sahip"
+        else:
+            return "Efsane"
+
+    def bilgi_göster(self):
+        unvan = self.get_unvan()
+        print(f"{self.char}'un puanı: {self.puan} ve unvanı: {unvan}")
+
+
