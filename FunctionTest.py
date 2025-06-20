55285f059d224f34a2e7c8e32c37b6e4a2736ee6
codebedo <bedirahan13@gmail.com>
2025-06-20 19:11:52 +0300

    try

--- FunctionTest.py
+++ FunctionTest.py
@@ -1,7 +1,8 @@
 class character:
-    def __init__(self, name, age):
+    def __init__(self, name, age, job):
         self.name = name
         self.age = age
+        self.job = job
 
 
     def say_hi(self):
@@ -11,11 +12,18 @@ class character:
     def talk_back(self):
         print(f"Hey  {self.name} whatsupp?")
 
+    def convert_secont(self):
+        print("How is the job")
+
+    def say_som(self):
+        print(f"{self.job} something is fun")
 
 
 
-k1 = character("Bedo", 24)
+k1 = character("Bedo", 24, "developer")
 k1.say_hi()
 k1.talk_back()
+k1.convert_secont()
+k1.say_som()
 
 
