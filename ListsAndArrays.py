56ecfcc209a0f7cdeb9222cb6bce0f6010f252bd
codebedo <bedirahan13@gmail.com>
2025-06-23 17:13:43 +0300

    list array loops

--- ListsAndArrays.py
+++ ListsAndArrays.py
@@ -1,4 +1,4 @@
-x = [9, 10,12, 12]
+"""x = [9, 10,12, 12]
 print(x)
 
 y = 0
@@ -8,5 +8,14 @@ while y <= 10:
     y += 1
 
 
-    print(x)
+    print(x)"""
+
+mine_Array = [12,5,7,8,8,1,2,8]
+checked_array = mine_Array[6]
+
+for i in mine_Array:
+    if i < checked_array:
+        checked_array =i
+
+print('Lowest Value', checked_array)
 
