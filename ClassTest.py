d0e54f46f71316f2ce99602e988a81232f78721c
codebedo <bedirahan13@gmail.com>
2025-06-23 16:56:39 +0300

    We find anotger way

--- ClassTest.py
+++ ClassTest.py
@@ -1,25 +1,24 @@
 class Title:
-    def __init__(self, name:str , age : int) ->None:
+    def __init__(self, name:str , age : int) :
         self.name = name
         self.age = age
 
 
 
-    def take_info(self)->None:
+    def take_info(self):
         User_name = self.name
         User_age = self.age
 
         User_name = input(print("Please enter the user name"))
         User_age = int(input("Please enter the user age"))
-    def show_info(self)->None:
 
-        print(f"{self.take_info()} , {self.age}")
-
+        print(f"{User_name} , {User_age}")
 
 
 
 
 
 
-k1 = Title(None, None)
-k1.show_info()
+
+k1 = Title(None,None)
+k1.take_info()
