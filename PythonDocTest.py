7332b19c9fbeb360948db7ea2e4d5fce0676c663
codebedo <bedirahan13@gmail.com>
2025-06-26 15:11:17 +0300

    test 4

--- PythonDocTest.py
+++ PythonDocTest.py
@@ -11,13 +11,23 @@ def f(arg=i):
     print(arg)
 i = 6
 f()"""
-
+"""
 
 def f(a, L=[]):
     L.append(a)
     return L
 
-
 print(f(1))
 print(f(2))
-print(f(3))
+print(f(3))"""
+
+
+def f_none(a, L=None):
+    if L is None:
+        L = []
+    L.append(a)
+    return L
+
+print(f_none(5))
+print(f_none(7))
+print(f_none(8))
