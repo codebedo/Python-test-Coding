d50432becbd000424cca090c6afadaed898e4813
codebedo <bedirahan13@gmail.com>
2025-06-26 15:07:25 +0300

    test 3

--- PythonDocTest.py
+++ PythonDocTest.py
@@ -1,7 +1,7 @@
 """the_world_is_flat = True
 
 if the_world_is_flat:
-    print("Be carefull the world is flat\n")"""
+    print("Be carefull the world is flat\n")
 
 
 
@@ -10,6 +10,14 @@ i = 5
 def f(arg=i):
     print(arg)
 i = 6
-f()
+f()"""
 
 
+def f(a, L=[]):
+    L.append(a)
+    return L
+
+
+print(f(1))
+print(f(2))
+print(f(3))
