f1edc0a4439fa568610197dd231b4155961cfccd
codebedo <bedirahan13@gmail.com>
2025-06-27 13:18:45 +0300

    Python for testing

--- PyTest.py
+++ PyTest.py
@@ -1,4 +1,24 @@
-for i in range(100):
-    print(i+1)
+class Test:
+    def __init__(self, target: int, start: int)->None:
+        self.target = target
+        self.start = start
 
-    
+
+
+    def for_loop(self):
+        starting = self.start
+        Target = self.target
+
+        for starting in range(Target):
+            print(self.start + 1)
+
+
+
+
+
+
+
+
+k1 = Test(10, 2)
+
+k1.for_loop()
