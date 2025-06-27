c0e9ef9494174df217cd183941c32bc424ad1787
codebedo <bedirahan13@gmail.com>
2025-06-27 19:31:16 +0300

    first hash map test

---
+++ HashMap.py
@@ -0,0 +1,14 @@
+hash_map ={
+    "ad": "Bedirhan",
+    "yaş" : "23",
+    "Meslek": "yazılımcı"
+
+
+}
+
+for i in hash_map:
+    print(hash_map[i])
+
+
+print(hash_map)
+print(hash_map["Meslek"])
