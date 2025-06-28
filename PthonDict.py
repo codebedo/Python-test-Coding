8b9c67838a8a1f0e037e5a53f535102eec38ce66
codebedo <bedirahan13@gmail.com>
2025-06-28 14:23:51 +0300

    This is list

--- PthonDict.py
+++ PthonDict.py
@@ -19,7 +19,26 @@ print(type(thisdict))
 # the dict() constructor to make a dicitonary
 
 
-thisdict = dict(name = "Bedo", age="23", Title="Developer")
+"""thisdict = dict(name = "Bedo", age="23", Title="Developer")
 df = pd.DataFrame(thisdict.items(), columns=["key", "Value"])
 
 print(df.to_string(index=False))
+"""
+
+all_example = ['A,1,1', 'B,2,1', 'C,4,4', 'D,4,5']
+
+
+lst = [
+    {"A":1, "B":2, "C":4, "D":4},
+    {"A":1, "B":2, "C":4, "D":5}
+
+]
+
+
+attributes = 'A,B,C'
+def get_Example():
+    for value in all_example:
+        yield  dict(zip(attributes, value.strip().replace("",'').split(',')))
+
+
+print(lst)
