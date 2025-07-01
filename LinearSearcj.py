9364f04c5435265d0d1e1010f523ab799b980fe1
codebedo <bedirahan13@gmail.com>
2025-07-01 17:56:49 +0300

    This is simple linear Search example

--- LinearSearcj.py
+++ LinearSearcj.py
@@ -1,8 +1,16 @@
-
+"""
 def linear_search(arr: int, key: int) -> int :
     for index, num in enumerate(arr):
         if num == key:
             return index
+
+    return -1"""
+
+def linear_search(arr: int, key: int) -> int:
+    for index, num in enumerate(arr):
+        if num == key:
+            return index
+
 
     return -1
 
@@ -10,9 +18,16 @@ arr = [23,323,43,5,4,23,321,90]
 key = 321
 
 
+res = linear_search(arr,key)
+
+if res != -1:
+    print(f"{key} found at index {res}")
+else:
+    print(f"this key {key} not found in the array")
+"""
 res = linear_search(arr, key)
 
 if res != -1:
     print(f"{key} found at index {res}")
 else:
-    print(f"{key} not found")
+    print(f"{key} not found")"""
