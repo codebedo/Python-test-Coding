4acb3ee56501563bdaeb30510f821ddca82c033a
codebedo <bedirahan13@gmail.com>
2025-07-02 13:16:36 +0300

    possible string

--- Search_wrog.py
+++ Search_wrog.py
@@ -1,11 +1,19 @@
-def possibleStringCount(word: str) -> int:
+"""def possibleStringCount(word: str) -> int:
     ans = 1
     for i in range(1, len(word)):
         if word[i] == word[i-1]:
             ans += 1
 
     return ans
+"""
 
+def possibleStringCount(word: str)-> int:
+    ans = 1
+    for i in range(1, len(word)):
+        if word[i] == word[i -1]:
+            ans += 1
+
+    return ans
 
 
 res = possibleStringCount("abcd")
