8c445bd38209e18d7e0c3b0482b28902ae1b65c2
codebedo <bedirahan13@gmail.com>
2025-07-04 16:17:41 +0300

    tring something nuw ::

--- Palindrome.py
+++ Palindrome.py
@@ -2,6 +2,13 @@ class Solution:
      def polindrome(self, num):
         self.num = num
 
+        reversed = int(str(self.num) [::-1])
+        print(reversed)
+        if num == reversed:
+            print(f"{self.num}palindrome")
+        else:
+            print("is not polindrome")
+"""
         i = num
 
         reversed = 0
@@ -14,7 +21,7 @@ class Solution:
             print(f"{self.num} is a palindrome number ")
 
         else:
-            print("sorry mate this is not")
+            print("sorry mate this is not")"""
 
 k1 = Solution()
-k1.polindrome(121)
+k1.polindrome(353)
