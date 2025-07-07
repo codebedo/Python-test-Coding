7726cddfdb7624d7e2d1e905a7e07e3c923da3cb
codebedo <bedirahan13@gmail.com>
2025-07-07 17:21:57 +0300

    this is algorithm of Merge sort

--- MergeSort.py
+++ MergeSort.py
@@ -1,26 +1,42 @@
-def merge(arr, left , mid, right):
-
-    n1 = mid - left +1
-    n2 = right - mid
-
-    #create temp arrays
-
-    L = [0] * n1
-    R = [0] * n2
-
-    # copy data to temp arrays  L[} and R[]
-
-    for i in range(n1):
-        L[i] = arr[left + i]
-    for j in range(n2):
-        R[j] = arr[mid + 1 + j]
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
 
 
-    i = 0  # initial index of first subarray
-    j = 0  # initial index of second subarray
-    k = left #initail index of merged subarray
+    # 1. bölme
+    mid = len(arr) //2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
 
 
-    # Merge the temp arrays back
-    # into arr [left..right
-    while i < n1 and j < n2:
+    # 2. birleştirme
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    merged = []
+
+    i = j = 0
+
+
+    # Karşılaştırarak birleştirme
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+
+    # kalanları ekleme
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+
+    return  merged
+# test
+
+
+
+arr = [38,27,43,3,9, 82,10]
+sorted_arr = merge_sort(arr)
+print("Sıralı liste: ", sorted_arr)
