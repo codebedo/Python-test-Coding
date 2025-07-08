965f2f4304ac875d186f18f45396fbdd129033d1
codebedo <bedirahan13@gmail.com>
2025-07-08 13:04:17 +0300

    this is func list tools all and any

--- inventory.py
+++ inventory.py
@@ -1,8 +1,8 @@
 inv = ["sword", None,None]
 
-if all(inv):
+if all(inv): # all ile kontrol edilebilyor , bu yeni bir liste fonksiyonu
     print('Inventory full!')
-elif any(inv):
+elif any(inv): # bir tane bile var mı kontrolü bu bizim için iyi bir fonksiyon
     print("Inventory has items!")
 else:
     print("Inventory empty")
