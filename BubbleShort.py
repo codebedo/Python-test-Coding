2873ab285c46099270e23d518a938c4d9159afa5
codebedo <bedirahan13@gmail.com>
2025-07-08 14:50:03 +0300

    this is for learning

--- BubbleShort.py
+++ BubbleShort.py
@@ -1,4 +1,4 @@
-def bubble_sort(arr):
+"""def bubble_sort(arr):
     n = len(arr)
 
     # dış döngü tüm listeyi kaç kez geçeceğmizi belirleer
@@ -7,7 +7,20 @@ def bubble_sort(arr):
         for j in range(0, n - i - 1):
             # ğer önceki eleman sonraki eleman dan büyük se yer dğiştir
             if arr[j] > arr[j + 1]:
-                arr[j] , arr[j + 1] = arr[j + 1] , arr[j] # python kolay değişim
+                arr[j] , arr[j + 1] = arr[j + 1] , arr[j] # python kolay değişim"""
+
+def buble_sort(arr):
+    n = len(arr)
+
+    # dış döngü listeyi kaç defa dolaşacğımızı belirler
+    for i in range(n):
+        print(arr)
+        # iç döngü her geçişte son i elemanı sıralandığı için daha az karşılaştır mayapacak
+        for j in range(0, n-  i -1):
+                
+            # eğer önceki elman sonraki ekemandan daha biyikse er değiştir
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 
 
@@ -16,6 +29,6 @@ def bubble_sort(arr):
 liste = [64, 34, 23, 12,22, 11, 90]
 print("Sıralama öncesi", liste)
 
-bubble_sort(liste)
+buble_sort(liste)
 
 print("Sıralama sonrası", liste)
