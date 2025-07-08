d678d3683d3f80ac1bccf3985ebaef604da5b861
codebedo <bedirahan13@gmail.com>
2025-07-08 12:33:01 +0300

    Thi is python tools of cycle

---
+++ Cycle.py
@@ -0,0 +1,15 @@
+import time
+from itertools import cycle
+
+
+lights = [
+    ('Green', 2),
+    ('Yellow', 0.5),
+    ('Red', 2)
+]
+
+colors = cycle(lights)
+while True:
+    c,s = next(colors)
+    print(c)
+    time.sleep(s)
