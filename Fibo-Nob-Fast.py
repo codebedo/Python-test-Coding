f9b64fccecfb2e41940532c8f5e6dc7ebd8ee5bd
codebedo <bedirahan13@gmail.com>
2025-07-09 15:01:40 +0300

    memoztion coding and repeating

--- Fibo-Nob-Fast.py
+++ Fibo-Nob-Fast.py
@@ -9,9 +9,41 @@ class solution:
 
         if n <= 1:
             return n
+        # burada dict olark saklıyor  #fibonacci tanimi yapıldı
         self.memo[n] = self.fib(n -1) + self.fib(n-2)
         return self.memo[n]
 
 
 sol = solution()
 print(sol.fib(10))
+
+
+
+class mysolution:
+    def __init__(self):
+        self.memo = {}
+
+
+    def fibo(self, n : int)-> int:
+        if n in self.memo:
+            return self.memo[n]
+        if n <= 1:
+            return n
+
+        self.memo[n] = self.fibo(n -1) + self.fibo(n-2)
+        return self.memo[n]
+
+
+class secondSolution:
+    def __init__(self):
+        self.memo = {} # amaç geçici dict oluşturmak
+
+
+    def fioo(self, n: int) -> int:
+        if n in self.memo:
+            return  self.memo[n]
+        if n <= 1:
+            return n
+
+        self.memo[n] = self.fioo(n-1) + self.fioo(n-2)
+        return self.memo[n]
