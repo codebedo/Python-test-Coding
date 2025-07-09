7b96492b00af977c510e643247184e5e007f0cb3
codebedo <bedirahan13@gmail.com>
2025-07-09 17:02:06 +0300

    we have practice for this

--- Python_strings/Strings_compare.py
+++ Python_strings/Strings_compare.py
@@ -2,8 +2,20 @@ word1 = "flower"
 word2 = "flow"
 
 
-for i in range(len(word1), len(word2)):
+for i in range(min(len(word1), len(word2))):
     print(f"{word1[i]} vs {word2[i]}")
     if word1[i] != word2[i]:
         print("Farkli karakter bulundu!")
-        break
+    break
+
+
+word3 = "aplle"
+word4 = "apricot"
+
+
+for i in range(max(len(word3), len(word4))):
+    print(f"{word3[i]} vs {word4[i]}")
+    if word3[i] != word4[i]:
+        print("What we have here")
+
+    break
