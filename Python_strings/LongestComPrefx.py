60e10eb48f4871684471eb4a69928fb7457e6551
codebedo <bedirahan13@gmail.com>
2025-07-10 19:35:42 +0300

    Example of compare str

--- Python_strings/LongestComPrefx.py
+++ Python_strings/LongestComPrefx.py
@@ -1,4 +1,4 @@
-strs = ["sunflower", "sunny", "sun"]
+"""strs = ["sunflower", "sunny", "sun"]
 
 
 prefix = strs[0]
@@ -10,5 +10,17 @@ for word in strs[1:]:
             break
 
 print(prefix)
+"""
+
+strc = ["apple", "apricot", "apcenter", "approach"]
 
 
+prefex = strc[0]
+
+for word in strc[1:]:
+    while not word.startswith(prefex):
+        prefex = prefex[:-1]
+        if prefex == "":
+            break
+
+print(prefex)
