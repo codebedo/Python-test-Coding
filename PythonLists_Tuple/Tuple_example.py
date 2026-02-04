8271056294c0a822ec53a8821699bfa10db8b32d
codebedo <bedirahan13@gmail.com>
2026-02-04 19:59:01 +0300

    this is for tumple example

---
+++ PythonLists_Tuple/Tuple_example.py
@@ -0,0 +1,12 @@
+messsage =(
+     "images/phonemomphone.png",
+    ["Selam", "akşam geliyorum"]
+
+)
+
+if messsage[0] != "images/phonemomphone.png":
+    for m in messsage[1]:
+        print(m)
+
+
+
